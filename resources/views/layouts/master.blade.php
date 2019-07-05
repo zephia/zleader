744ff47d16aa8351cd3e996f80d92100290cd137
Cristian Jimenez <cristian@zephia.com.ar>
2019-07-05 18:26:53 +0000

    Title heritage fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,3 +1,6 @@
+@section('title')
+  zLeader | Lead router and dashboard
+@stop
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -7,7 +10,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('title', 'zLeader | Lead router and dashboard')</title>
+  <title>@yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.6 -->
